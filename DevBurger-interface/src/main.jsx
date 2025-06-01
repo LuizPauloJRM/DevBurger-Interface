81e3cd046872b5350f3a962027815465665c832e
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2025-06-01 14:32:44 -0300

    eslint : Configurações

--- DevBurger-interface/src/main.jsx
+++ DevBurger-interface/src/main.jsx
@@ -1,5 +1,6 @@
 import { React } from 'react'
 import { ReactDOM } from 'react-dom/client'
+
 import { Login } from './containers/Login/index'  
 
 React.DOM.createRoot(document.getElementById('root')).render(
