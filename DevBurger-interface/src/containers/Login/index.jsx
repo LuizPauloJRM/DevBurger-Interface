23a933dbef54c0ab7fcd6cb1bef9f4aa14411881
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2025-06-02 00:00:32 -0300

    Estrutura do login

--- DevBurger-interface/src/containers/Login/index.jsx
+++ DevBurger-interface/src/containers/Login/index.jsx
@@ -1,8 +1,31 @@
-export function Login(){
+import Logo from '../../assets/logo.svg'
+import { Button, Container, Form, InputContainer, LeftContainer, Link, RightContainer, Title } from "./styles";
+
+
+export function Login() {
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
-       
+        <Container>
+            <LeftContainer>
+                <img src={Logo} alt="logo-devburger" />
+            </LeftContainer>
+            <RightContainer>
+                <Title>
+                    Bem vindo ao <span>Dev Burguer</span> Acesse
+                </Title>
+                <Form>
+                    <InputContainer>
+                        <label>Email</label>
+                        <input type="email" />
+                    </InputContainer>
+                    <InputContainer>
+                        <label>Senha</label>
+                        <input type="password" />
+                    </InputContainer>
+                    <Link>Esqueci minha senha</Link>
+                    <Button>Entrar</Button>
+                    <Link>Não possui conta e deseja criar a sua? Clique aqui e faça seu cadastro</Link>
+                </Form>
+            </RightContainer>
+        </Container>
     );
 }
